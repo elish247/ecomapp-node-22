e22c8ab2cf17847b4bc12251cda19c4630218620
Tejas <tejas14all@gmail.com>
2022-02-15 09:35:08 +0530

    role controller and db connection

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const sessionController = require("./controller/session-controller")
+const roleController = require("./controller/role-controller")
+
 
 
 const app = express()
@@ -7,6 +11,18 @@ const app = express()
 app.use(express.json()) //mobile -> accept json data from request and set data into body 
 app.use(express.urlencoded({extended:true})) //web --> accept url encoded data from request and set data into body  
 
+
+//database 
+mongoose.connect('mongodb://localhost:27017/ecom',function(err){
+  if(err){
+    console.log("db connection fai .. .. . ");
+    console.log(err);
+  }else{
+    console.log("db Connected....");
+  }
+})
+
+//urls 
 
 app.get("/",function(req,res){
     res.write("welcome...")
@@ -17,6 +33,13 @@ app.get("/",function(req,res){
 app.get("/login",sessionController.login)
 app.get("/signup",sessionController.signup) 
 app.post("/saveuser",sessionController.saveuser)
+
+
+//role 
+app.post("/roles",roleController.addRole)
+
+
+//server 
 
 app.listen(3000,function(){
   console.log("server started on 3000");  
